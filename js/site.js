1c5cf4df2f5ce45c131ae597d12db6961ec0e9c7
jduncanator <jduncanator@hotmail.com>
2014-03-17 18:09:38 +1100

    Updated site to new host
    Updated download URLs to new download host. Updated version numbers. Added 32/64 bit Linux detection. Removed Google Analytics (for now)

--- js/site.js
+++ js/site.js
@@ -1,11 +1,25 @@
-var userAgent = navigator.platform;
+var userAgent = navigator.platform,
+    fileHost = "http://static.cdnjd.com/releases/popcorn/";
 
-if(userAgent.substring(0,3) == "Mac") {
+var mac = /Mac/,
+    win = /Win/,
+    lin = /Lin/,
+    lin64 = /x86_64/;
+
+Array.prototype.forEach.call(document.querySelectorAll('[data-file]'), function(el) {
+  el.href = fileHost + el.getAttribute('data-file');
+});
+
+if(mac.test(userAgent)) {
 	document.getElementsByTagName('body')[0].className+=' mac';
-} else if (userAgent.substring(0,3) == "Win") {
+} else if (win.test(userAgent)) {
 	document.getElementsByTagName('body')[0].className+=' win';
-} else if (userAgent.substring(0,3) == "Lin") {
-	document.getElementsByTagName('body')[0].className+=' lin';
+} else if (lin.test(userAgent)) {
+  if(lin64.test(userAgent)) {
+    document.getElementsByTagName('body')[0].className+=' lin-64';
+  } else {
+    document.getElementsByTagName('body')[0].className+=' lin-32';
+  }
 } else {
 	document.getElementsByTagName('body')[0].className+=' nope';
 }
