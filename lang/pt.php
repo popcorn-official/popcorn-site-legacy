7532f8c8cfb1b13a1761e47061aa59d4d4fab0fb
SrPatinhas <migas_cerejo-1993@hotmail.com>
2014-04-13 20:52:49 +0100

    add Portugues translation - pt.php

---
+++ lang/pt.php
@@ -0,0 +1,34 @@
+<?php
+$langsite = Array(
+    // Descrição da barra de titulo
+    "TITLE_DESC" => "Veja filmes por torrent rápidamente",
+    "SITE_DESC" => "Esqueça as transferências! Veja os melhores filmes rápidamente em HD, com legendas, grátis! Disponivél para Windows, Mac e Linux.",
+    "HEADER" => "Veja filmes por torrent rápidamente",
+    "SUBHEADER" => "Atualmente em Beta, mas vá em frente e tente!",
+    // %s Repor com a versão atual
+    "BTN_DOWNLOAD" => "Transferir Beta %s",
+    "DL_DESC_WIN" => "Para Windows 7 e superiores",
+    "DL_DESC_MAC" => "Para Mac OSX 10.6 e superiores",
+    "DL_DESC_LIN32" => "Para utilizadores de Linux 32-bit",
+    "DL_DESC_LIN64" => "Para utilizadores de Linux 64-bit",
+    "DISCLAIMER_TITLE" => "Popcorn Time transmite filmes através de Torrents",
+    "DISCLAIMER_DESC" => "Trasnferir conteúdo protegido por direitos autorais pode ser ilegal no seu pais. Use por sua conta em risco.",
+    "FEAT_MOVIES" => "Bons filmes",
+    "FEAT_MOVIES_DESC" => "Nós estamos constantemente há procura por toda a internet pelos melhores torrents nos melhores sites.",
+    "FEAT_NORESTRICTION" => "Sem restrições",
+    "FEAT_NORESTRICTION_DESC"=> "Veja qualquer filme as vezes que quiser. Tudo o que precisa é uma ligação á internet adequada.",
+    "FEAT_CATALOGUE" => "Excelente catálogo",
+    "FEAT_CATALOGUE_DESC" => "Se o filme está por ai, Popcorn Time irá encontrar a melhor versão possível e começar a transmitir logo de seguida.",
+    "FEAT_QUALITY" => "A melhor qualidade",
+    "FEAT_QUALITY_DESC" => "Veja os melhores filmes rápidamente em HD e com legendas. E depois continue a assistir.",
+    "FEAT_BEST" => "O melhor de tudo... é grátis!",
+    "HOW_TITLE" => "Dificil de acreditar?",
+    "HOW_DESC" => "Veja como trabalha fácilmente...",
+    "HOW_1" => "Abra PT & sescolha o filme",
+    "HOW_2" => "Escolha HD & Legendas",
+    "HOW_3" => "Carregar Ver & Desfrute!",
+    "GET_TITLE" => "Uma nova maneira de ver filmes",
+    "GET_DESC" => "Apenas cuide do popcorn & deixe o resto connosco.",
+    "FOOTER" => "Feito com <span>&lt;3</span> por um grupo de 'geeks' de Todo O Mundo"
+);
+?>
