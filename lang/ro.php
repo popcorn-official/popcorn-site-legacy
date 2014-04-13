226356515f9fa0dc4add493d3ad56abbd56a2638
PatruSorin <patrusorin95@yahoo.com>
2014-04-14 00:29:36 +0300

    Create ro.php

---
+++ lang/ro.php
@@ -0,0 +1,34 @@
+<?php
+$langsite = Array(
+    // Site description in the title bar
+    "TITLE_DESC"             => "Priveşte filme de pe torrente instantaneu",
+    "SITE_DESC"              => "Nu mai descărca filme! Priveşte cele mai bune filme instantaneu in HD, cu subtitrari, gratuit! Disponibil pentru Windows, Mac şi Linux.",
+    "HEADER"                 => "Priveşte filme de pe torrente instantaneu",
+    "SUBHEADER"              => "Momentan în Beta, dar te rugăm să îl încerci!",
+    // %s replaced with version number
+    "BTN_DOWNLOAD"           => "Descarcă Beta %s",
+    "DL_DESC_WIN"            => "Pentru Windows 7 şi superior",
+    "DL_DESC_MAC"            => "Pentru Mac OSX 10.6 şi superior",
+    "DL_DESC_LIN32"          => "Pentru utilizatori de Linux 32-bit",
+    "DL_DESC_LIN64"          => "Pentru utilizatori de Linux 64-bit",
+    "DISCLAIMER_TITLE"       => "Popcorn Time permite vizionarea de filme prin Torrente",
+    "DISCLAIMER_DESC"        => "Descarcarea de materiale cu drepturi de autor s-ar putea să fie ilegală in ţara dumneavoastră. Utilizaţi pe propria răspundere.",
+    "FEAT_MOVIES"            => "Filme grozave",
+    "FEAT_MOVIES_DESC"       => "Căutăm continuu pe internet cele mai bune torrente de la cele mai importante siteuri",
+    "FEAT_NORESTRICTION"     => "Fără restricţi", 
+    "FEAT_NORESTRICTION_DESC"=> "Priveşte orice filme doreşti de cate ori doreşti. Singurul lucru de care ai nevoie este o conexiune la internet.",
+    "FEAT_CATALOGUE"         => "Colecţie de filme variată", 
+    "FEAT_CATALOGUE_DESC"    => "Daca filmul se afla pe internet Popcorn Time va găsi cea mai bună versiune posibilă si va începe sa ruleze filmul instantaneu.",
+    "FEAT_QUALITY"           => "Cea mai bună calitate", 
+    "FEAT_QUALITY_DESC"      => "Priveşte orice film, subtitrat, în HD. ",
+    "FEAT_BEST"              => "Şi cel mai bun lucru... este gratuit!", 
+    "HOW_TITLE"              => "Greu de crezut?",
+    "HOW_DESC"               => "Vezi cat de uşor funcţionează...",
+    "HOW_1"                  => "Deschide PT & selectează un film",
+    "HOW_2"                  => "Alege HD & Subtitrarea",
+    "HOW_3"                  => "Apasa play & bucurăte de film!",
+    "GET_TITLE"              => "Un nou mod de a viziona filme",
+    "GET_DESC"               => "Dute si faţi un bol de popcorn & lasă restul pe seama noastră.",
+    "FOOTER"                 => "Făcut cu <span>&lt;3</span> de un grup de programatori din Jurul Lumii"
+);
+?>
