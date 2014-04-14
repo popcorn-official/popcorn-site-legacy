8fd0d179db4303d45c223c22d4eb310c88df154c
PatruSorin <patrusorin95@yahoo.com>
2014-04-14 18:35:18 +0300

    Update ro.php

--- lang/ro.php
+++ lang/ro.php
@@ -2,7 +2,7 @@
 $langsite = Array(
     // Site description in the title bar
     "TITLE_DESC"             => "Priveşte filme de pe torrente instantaneu",
-    "SITE_DESC"              => "Nu mai descărca filme! Priveşte cele mai bune filme instantaneu in HD, cu subtitrari, gratuit! Disponibil pentru Windows, Mac şi Linux.",
+    "SITE_DESC"              => "Nu mai descărca filme! Priveşte cele mai bune filme instantaneu in HD, cu subtitrări, gratuit! Disponibil pentru Windows, Mac şi Linux.",
     "HEADER"                 => "Priveşte filme de pe torrente instantaneu",
     "SUBHEADER"              => "Momentan în Beta, dar te rugăm să îl încerci!",
     // %s replaced with version number
@@ -16,14 +16,14 @@ $langsite = Array(
     "FEAT_MOVIES"            => "Filme grozave",
     "FEAT_MOVIES_DESC"       => "Căutăm continuu pe internet cele mai bune torrente de la cele mai importante siteuri",
     "FEAT_NORESTRICTION"     => "Fără restricţi", 
-    "FEAT_NORESTRICTION_DESC"=> "Priveşte orice filme doreşti de cate ori doreşti. Singurul lucru de care ai nevoie este o conexiune la internet.",
+    "FEAT_NORESTRICTION_DESC"=> "Priveşte orice film doreşti de cate ori doreşti. Singurul lucru de care ai nevoie este o conexiune la internet.",
     "FEAT_CATALOGUE"         => "Colecţie de filme variată", 
-    "FEAT_CATALOGUE_DESC"    => "Daca filmul se afla pe internet Popcorn Time va găsi cea mai bună versiune posibilă si va începe sa ruleze filmul instantaneu.",
+    "FEAT_CATALOGUE_DESC"    => "Daca filmul se afla pe internet Popcorn Time va găsi cea mai bună versiune posibilă si va începe să ruleze filmul instantaneu.",
     "FEAT_QUALITY"           => "Cea mai bună calitate", 
     "FEAT_QUALITY_DESC"      => "Priveşte orice film, subtitrat, în HD. ",
     "FEAT_BEST"              => "Şi cel mai bun lucru... este gratuit!", 
     "HOW_TITLE"              => "Greu de crezut?",
-    "HOW_DESC"               => "Vezi cat de uşor funcţionează...",
+    "HOW_DESC"               => "Vezi căt de uşor funcţionează...",
     "HOW_1"                  => "Deschide PT & selectează un film",
     "HOW_2"                  => "Alege HD & Subtitrarea",
     "HOW_3"                  => "Apasa play & bucurăte de film!",
