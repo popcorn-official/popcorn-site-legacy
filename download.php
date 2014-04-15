ab54a220a8012ea490708d3ca836aeec742633a7
David Lemarier <david@reallysuccessful.com>
2014-04-15 10:27:34 -0400

    Update download link and home page for 2.9

--- download.php
+++ download.php
@@ -1,10 +1,10 @@
 <?php
 	
 	$releases = Array(
-		"mac"		=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.8.2-Mac.tar.gz",
-		"win32"		=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.8.2-Win32.zip",
-		"linux32"	=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.8.2-Linux-32.tar.gz",
-		"linux64"	=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.8.2-Linux-64.tar.gz"
+		"mac"		=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Mac.tar.gz",
+		"win32"		=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Win-32.zip",
+		"linux32"	=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Linux-32.tar.gz",
+		"linux64"	=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Linux-64.tar.gz"
 	);
 
 	if (isset($_GET['os'])) {
