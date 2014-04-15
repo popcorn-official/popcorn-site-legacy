1fb1cd4028cc3794da0b3c10a3a0ed903fc0edcf
Foxbot <frederic.godfrindelmont@gmail.com>
2014-04-15 21:17:02 +0200

    Cleanup
    HTML cleanup
    Use constant instead of variable
    TOS design

--- faq.php
+++ faq.php
@@ -1,11 +1,11 @@
-<?php include("inc/config.php"); ?>
+<?php require_once(__DIR__ . "/inc/config.php"); ?>
 <!DOCTYPE HTML>
 <html>
 <head>
-	<meta charset="utf-8" />
-	<link rel="canonical" href="<?=$domain?>" />
-	<link rel="stylesheet" type="text/css" href="<?=$domain?>css/style.css" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta charset="utf-8">
+	<link rel="canonical" href="<?=DOMAIN?>faq">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!--
                                                         .,;+###+;:.
                                                   .##;`             .'@:
@@ -57,31 +57,28 @@
                                     :@;     `'++#@@##';,.`
                                        ;,.`                                                          -->
 									   
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-	<link rel="icon" href="favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/images/favicon.ico" type="image/x-icon">
 
-	<title>Popcorn Time - <?=$langsite["TITLE_FAQ"]?></title>
-	<meta name="description" content="<?=$langsite["SITE_DESC"]?>" />
-	<link rel="image_src" href="<?=$domain?>images/header-ui.jpg" />
-	
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
-    <script src="<?=$domain?>js/jquery.polyglot.language.switcher.js" type="text/javascript"></script>
+	<title><?=$langsite["TITLE_FAQ"]?> - Popcorn Time</title>
+	<meta name="description" content="<?=$langsite["SITE_DESC"]?>">
+	<link rel="image_src" href="/images/header-ui.jpg">
 
-	<meta property="og:title" content="Popcorn Time" />
-	<meta property="og:type" content="website" />
-	<meta property="og:image" content="<?=$domain?>images/header-ui.jpg" />
-	<meta property="og:url" content="<?=$domain?>" />
-	<meta property="og:site_name" content="Popcorn Time" />
+	<meta property="og:title" content="Popcorn Time">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="/images/header-ui.jpg">
+	<meta property="og:url" content="<?=DOMAIN?>">
+	<meta property="og:site_name" content="Popcorn Time">
 </head>
-<body id="page-faq" class="beta">
+<body class="beta page">
 	<section id="header">
 		<nav class="fixed-width fadeInDown wow">
-			<a href="<?=$domain?>" id="logo">Popcorn Time</a>
+			<a href="<?=LINK_HOME?>" id="logo">Popcorn Time</a>
 			<ul class="align-right">
-				<!--<li class="social"><a href="<?=$link_fb?>" target="_blank" class="icon-facebook"></a></li>-->
-				<li class="social"><a href="<?=$link_bl?>" target="_blank" class="icon-ghost"></a></li>
-				<li class="social"><a href="<?=$link_tt?>" target="_blank" class="icon-twitter"></a></li>
-				<li class="social"><a href="<?=$link_gh?>" target="_blank" class="icon-github"></a></li>
+				<?php if(defined('LINK_FCBK')) { ?><li class="social"><a href="<?=LINK_FCBK?>" target="_blank" class="icon-facebook"></a></li><?php } ?>
+				<?php if(defined('LINK_BLOG')) { ?><li class="social"><a href="<?=LINK_BLOG?>" target="_blank" class="icon-ghost"></a></li><?php } ?>
+				<?php if(defined('LINK_TWTR')) { ?><li class="social"><a href="<?=LINK_TWTR?>" target="_blank" class="icon-twitter"></a></li><?php } ?>
+				<?php if(defined('LINK_GTHB')) { ?><li class="social"><a href="<?=LINK_GTHB?>" target="_blank" class="icon-github"></a></li><?php } ?>
 			</ul>
 
 			<ul class="align-left">
@@ -92,16 +89,16 @@
 						</select>
 					</form>
 				</div>
-				<li class="text"><a href="faq">FAQ</a></li>
-				<li class="text"><a href="tos">TOS</a></li>
+				<li class="text"><a href="<?=LINK_HOME?>">HOME</a></li>
+				<li class="text"><a href="<?=LINK_TOS?>">TOS</a></li>
 			</ul>
 		</nav>
 	</section>
 
 	<div class="container fadeIn wow">
 		<section id="faq" class="fixed-width">
-			<p class="warning"><?=$langfaq["WARNING_1"]?> <a href="<?=$domain?>"><?=$langfaq["WARNING_2"]?></a><br/>
-			<?=$langfaq["WARNING_3"]?> <a href="<?=$domain?>">get-popcorn.com</a> <?=$langfaq["WARNING_4"]?></p>
+			<p class="warning"><?=$langfaq["WARNING_1"]?> <a href="<?=DOMAIN?>"><?=$langfaq["WARNING_2"]?></a><br>
+			<?=$langfaq["WARNING_3"]?> <a href="<?=DOMAIN?>"><?=DOMAIN?></a> <?=$langfaq["WARNING_4"]?></p>
 
 			<article id="what">
 				<h1><?=$langfaq["TITLE"]?></h1>
@@ -141,8 +138,8 @@
 
 			<article id="cant-launch">
 				<h2><?=$langfaq["LAUNCH_Q"]?></h2>
-				<p class="icon-mac"><strong><?=$langfaq["LAUNCH_Q1"]?></strong><br /><?=$langfaq["LAUNCH_A1"]?></p>
-				<p class="icon-mac"><strong><?=$langfaq["LAUNCH_Q2"]?></strong><br />
+				<p class="icon-mac"><strong><?=$langfaq["LAUNCH_Q1"]?></strong><br><?=$langfaq["LAUNCH_A1"]?></p>
+				<p class="icon-mac"><strong><?=$langfaq["LAUNCH_Q2"]?></strong><br>
 					<?=$langfaq["LAUNCH_A2"]?>
 					<ul class="step-code">
 						<li><span class="code">$ chmod +x /Applications/Popcorn\ Time.app/Contents/MacOS/node-webkit</span></li>
@@ -183,21 +180,24 @@
 		<section id="footer" class="fadeInUp wow">
 			<hr style="display: none">
 			<nav class="clear-fix">
-				<a href="/" class="logo">Popcorn time</a>
+				<a href="<?=LINK_HOME?>" class="logo">Popcorn time</a>
 				<ul>
-					<!--<li class="social"><a href="<?=$link_fb?>" target="_blank" class="icon-facebook"> </a></li>-->
-					<li class="social"><a href="<?=$link_tt?>" target="_blank" class="icon-twitter"> </a></li>
-					<li class="github"><a href="<?=$link_gh?>" target="_blank" class="icon-github">Github</a></li>
+					<?php if(defined('LINK_FCBK')) { ?><li class="social"><a href="<?=LINK_FCBK?>" target="_blank" class="icon-facebook"></a></li><?php } ?>
+					<?php if(defined('LINK_BLOG')) { ?><li class="social"><a href="<?=LINK_BLOG?>" target="_blank" class="icon-ghost"></a></li><?php } ?>
+					<?php if(defined('LINK_TWTR')) { ?><li class="social"><a href="<?=LINK_TWTR?>" target="_blank" class="icon-twitter"></a></li><?php } ?>
+					<?php if(defined('LINK_GTHB')) { ?><li class="github"><a href="<?=LINK_GTHB?>" target="_blank" class="icon-github">Github</a></li><?php } ?>
                 </ul>
 			</nav>
 			<footer>
 				<hr>
 				<i class="film"></i>
-				<p><?=$langsite["FOOTER"]?><br /> <small><a href="<?=$domain?>tos">Terms of Service</a> — <a href="<?=$domain?>faq">FAQ</a></small></p>
+				<p><?=$langsite["FOOTER"]?><br> <small><a href="<?=LINK_TOS?>">Terms of Service</a> — <a href="<?=LINK_FAQ?>">FAQ</a></small></p>
 			</footer>
 		</section>
 	</div>
-	<script type="text/javascript" src="<?=$domain?>js/wow.js"></script>
-	<script type="text/javascript" src="<?=$domain?>js/site.js"></script>
+	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+	<script type="text/javascript" src="/js/wow.js"></script>
+	<script type="text/javascript" src="/js/site.js"></script>
+    <script type="text/javascript" src="/js/jquery.polyglot.language.switcher.js"></script>
 </body>
 </html>
--- inc/config.php
+++ inc/config.php
@@ -1,41 +1,50 @@
 <?php
+	define('DOMAIN', 		'http://get-popcorn.com/');
+	define('VERSION',		'2.9');
+	
+	/* navigation */
+	define('LINK_HOME',		'/');
+	define('LINK_FAQ',		'/faq');
+	define('LINK_TOS',		'/tos');
+	define('LINK_DL',		'/download');
+	
+	/* social */
+	// define('LINK_FCBK',		'https://facebook.com/getpopcorntime');
+	define('LINK_TWTR',		'https://twitter.com/popcorntimetv');
+	define('LINK_GTHB',		'http://github.com/popcorn-official');
+	define('LINK_BLOG',		'http://blog.get-popcorn.com/');
 
-$domain       = "http://get-popcorn.com/";
-$link_win     = "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Win-32.zip";
-$link_mac     = "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Mac.tar.gz";
-$link_linux32 = "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Linux-32.tar.gz";
-$link_linux64 = "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Linux-64.tar.gz";
-$link_version = "2.9";
-$link_fb      = "https://facebook.com/getpopcorntime";
-$link_bl      = "http://blog.get-popcorn.com/";
-$link_tt      = "https://twitter.com/popcorntimetv";
-$link_gh      = "http://github.com/popcorn-official";
-
-// Language manager
-$langlist = array();
-foreach(scandir("inc/lang") as $value) {
-	if(preg_match("/^([a-z-]{2,5}).php$/i", $value, $langname)){
-	    $langlist[]=$langname[1];
+	/* download */
+	$releases = array(
+		"mac"		=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Mac.tar.gz",
+		"win"		=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Win-32.zip",
+		"linux32"	=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Linux-32.tar.gz",
+		"linux64"	=> "http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Linux-64.tar.gz"
+	);
+	
+	/* language manager */
+	$langlist = array();
+	foreach(scandir("inc/lang") as $value) {
+		if(preg_match("/^([a-z-]{2,5}).php$/i", $value, $langname)){
+			$langlist[]=$langname[1];
+		}
 	}
-}
-$lang = "en";
-if (!empty($_GET["lang"])) {
-    $newlang = $_GET["lang"];
-} else if (!empty($_COOKIE["lang"])) {
-    $newlang = $_COOKIE["lang"];
-}
-if (isset($newlang) && preg_match("/^[a-z-]{2,5}$/i", $newlang) && in_array($newlang, $langlist)) {
-    $lang = $newlang;
-    setcookie("lang", $lang,time()+31536000);
-}
-if(!empty($_GET["lang"])) header("Location: ".strtok($_SERVER['REQUEST_URI'], '?'));
-include("inc/lang/$lang.php");
-
-function printlangs(){
-	global $langlist,$lang;
-	foreach($langlist as $langname){
-		echo "<option id='$langname' value='$langname' ".($langname == $lang? 'selected':'').">&nbsp;</option>\n";
+	$lang = "en";
+	if (!empty($_GET["lang"])) {
+		$newlang = $_GET["lang"];
+	} else if (!empty($_COOKIE["lang"])) {
+		$newlang = $_COOKIE["lang"];
 	}
-}
+	if (isset($newlang) && preg_match("/^[a-z-]{2,5}$/i", $newlang) && in_array($newlang, $langlist)) {
+		$lang = $newlang;
+		setcookie("lang", $lang,time()+31536000);
+	}
+	if(!empty($_GET["lang"])) header("Location: ".strtok($_SERVER['REQUEST_URI'], '?'));
+	include("inc/lang/$lang.php");
 
-?>
+	function printlangs(){
+		global $langlist,$lang;
+		foreach($langlist as $langname){
+			echo "<option id='$langname' value='$langname' ".($langname == $lang? 'selected':'').">&nbsp;</option>\n";
+		}
+	}
